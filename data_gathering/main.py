44b0303505f15ebebc88c77e3c1de9a35eae814c
FRYoussef <f.r.youssef@hotmail.com>
2021-09-28 16:51:14 +0200

    Fixed data collection error

--- data_gathering/main.py
+++ data_gathering/main.py
@@ -9,13 +9,13 @@ def extract_data_from(root: str, system: str) -> pd.DataFrame:
 
     for f in files:
         params = f.split('.')
-        d = {
-            'system': system,
-            'k': params[5],
-            'device': params[1],
-            'precision': params[2],
-            'dimension': f'{params[3]}x{params[4]}',
-        }
+        d = dict(
+            system=system,
+            k=params[5],
+            device=params[1],
+            precision=params[2],
+            dimension=f'{params[3]}x{params[4]}',
+        )
 
         with open(os.path.join(path, f), 'r') as fin:
             content: str = fin.read()
@@ -28,8 +28,8 @@ def extract_data_from(root: str, system: str) -> pd.DataFrame:
             time = int(time / 1000) # ms -> s
             d['kernel'] = name
             d['time'] = time
-
-        tests.append(d)
+            aux: Dict = d.copy()
+            tests.append(aux)
 
     return pd.DataFrame(tests)
 
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     systems: List[str] = [s for s in os.listdir(path) if os.path.isdir(os.path.join(path, s))]
 
     for s in systems:
-        df = df.append(extract_data_from(path, s), ignore_index=True)
+        df = df.append(extract_data_from(path, s))
 
     out: str = os.path.join(path, 'system_times.csv')
     df.to_csv(out, index=False)
