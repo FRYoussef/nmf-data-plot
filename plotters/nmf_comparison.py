96289481c29cce126af81f707cfd6a9729063830
FRYoussef <f.r.youssef@hotmail.com>
2022-07-15 11:44:00 +0200

    modified:   .gitignore 	modified:   data_gathering/main.py 	deleted:    datawarehouse/system/16063x280x4_nmf_comparison.eps 	deleted:    datawarehouse/system/5000x38x4_nmf_comparison.eps 	deleted:    datawarehouse/system/54675x1973x4_nmf_comparison.eps 	modified:   datawarehouse/system/desktop/nmf.desktop.uhd630.openmp.simple.16063.280.4 	modified:   datawarehouse/system/desktop/nmf.desktop.uhd630.openmp.simple.54675.1973.4 	modified:   datawarehouse/system/desktop/nmf.desktop.uhd630.sycl.simple.16063.280.4 	modified:   datawarehouse/system/desktop/nmf.desktop.uhd630.sycl.simple.54675.1973.4 	modified:   datawarehouse/system/devcloud/nmf.devcloud.dg1.openmp.simple.16063.280.4 	modified:   datawarehouse/system/devcloud/nmf.devcloud.dg1.openmp.simple.54675.1973.4 	modified:   datawarehouse/system/devcloud/nmf.devcloud.dg1.sycl.simple.16063.280.4 	modified:   datawarehouse/system/devcloud/nmf.devcloud.dg1.sycl.simple.54675.1973.4 	deleted:    datawarehouse/system/system_times.csv 	new file:   datawarehouse/system/volta1/nmf.volta1.rtx3090.cuda.simple.16063.280.4 	new file:   datawarehouse/system/volta1/nmf.volta1.rtx3090.cuda.simple.54675.1973.4 	new file:   datawarehouse/system/volta1/nmf.volta1.rtx3090.openmp.simple.16063.280.4 	new file:   datawarehouse/system/volta1/nmf.volta1.rtx3090.openmp.simple.54675.1973.4 	new file:   datawarehouse/system/volta1/nmf.volta1.rtx3090.sycl.simple.16063.280.4 	new file:   datawarehouse/system/volta1/nmf.volta1.rtx3090.sycl.simple.54675.1973.4 	modified:   plotters/nmf_comparison.py

--- plotters/nmf_comparison.py
+++ plotters/nmf_comparison.py
@@ -6,11 +6,28 @@ import numpy as np
 
 def plot(out_path: str, df: pd.DataFrame) -> None:
     #ordering
-    df.loc[(df.device == 'dg1'), 'device'] = 'iris-dg1'
+    df.loc[(df.device == 'i7-10700'), 'device'] = '1'
+    df.loc[(df.device == 'i9-10920X'), 'device'] = '2'
+    df.loc[(df.device == 'gold'), 'device'] = '3'
+    df.loc[(df.device == 'uhd630'), 'device'] = '4'
+    df.loc[(df.device == 'dg1'), 'device'] = '5'
+    df.loc[(df.device == 'ats'), 'device'] = '6'
+    df.loc[(df.device == 'rtx3090'), 'device'] = '7'
     df = df.sort_values(by=['system', 'device', 'version'], ascending=False)
 
-    df.loc[(df.device == 'iris-dg1'), 'device'] = 'Xe DG1'
-    df.loc[(df.device == 'uhd630'), 'device'] = 'UHD 630'
+    df.loc[(df.device == '1'), 'device'] = 'i7-10700'
+    df.loc[(df.device == '2'), 'device'] = 'i9-10920X'
+    df.loc[(df.device == '3'), 'device'] = 'Xeon Gold 6336Y'
+    df.loc[(df.device == '4'), 'device'] = 'UHD 630'
+    df.loc[(df.device == '5'), 'device'] = 'Xe DG1'
+    df.loc[(df.device == '6'), 'device'] = 'Xe HP Artic Sound'
+    df.loc[(df.device == '7'), 'device'] = 'RTX 3090'
+
+    cpu: List[str] = ['i7-10700', 'i9-10920X', 'Xeon Gold 6336Y']
+    gpu: List[str] = ['UHD 630', 'Xe DG1', 'Xe HP Artic Sound', 'GT 1030', 'RTX 3090']
+
+    #filter by cpu or gpu
+    df = df[df['device'].isin(gpu)]
 
     df['system'] = df['system'].str.capitalize()
 
@@ -18,7 +35,7 @@ def plot(out_path: str, df: pd.DataFrame) -> None:
     df.loc[(df.version == 'OPENMP'), 'version'] = 'OpenMP'
     df.loc[(df.version == 'BASE_CODE'), 'version'] = 'BLAS base'
 
-    df['name'] = '[' + df['version'] + '] ' + df["system"] + ' ' + df["device"]
+    df['name'] = '[' + df['version'] + '] ' + ' ' + df["device"]
     
     fig, ax = plt.subplots(figsize=(10,10))
 
@@ -33,17 +50,17 @@ def plot(out_path: str, df: pd.DataFrame) -> None:
         
         margin += values
 
-    # bar texture
-    bars = ax.patches
-    start = 0
-    for _ in range(4):
-        bs = bars[start:start+10]
-        start += 10
-        for i, b in enumerate(bs):
-            if i < 5:
-                b.set_hatch('x')
-            else:
-                b.set_hatch('//')
+    # # bar texture
+    # bars = ax.patches
+    # start = 0
+    # for _ in range(4):
+    #     bs = bars[start:start+10]
+    #     start += 10
+    #     for i, b in enumerate(bs):
+    #         if i < 5:
+    #             b.set_hatch('x')
+    #         else:
+    #             b.set_hatch('//')
     
 
     #legend
@@ -57,11 +74,11 @@ def plot(out_path: str, df: pd.DataFrame) -> None:
     plt.xlabel('Seconds', fontsize=20)
     plt.ylabel('')
     ax.tick_params(axis='both', which='major', labelsize=15)
-    x_limit = 32
+    x_limit = 28
     ax.set_xlim(0, x_limit)
     plt.xticks(np.arange(0, x_limit, 2))
 
-    plt.savefig(out_path, format='eps', bbox_inches='tight')
+    plt.savefig(out_path, format='png', bbox_inches='tight')
 
 
 if __name__ == '__main__':
@@ -74,6 +91,6 @@ if __name__ == '__main__':
     df2: pd.DataFrame = df[df['matrix_size'] == '16063x280']
     df3: pd.DataFrame = df[df['matrix_size'] == '54675x1973']
     
-    #plot(out_path=os.path.join(data_path, '5000x38x4_nmf_comparison.eps'), df=df1)
-    plot(out_path=os.path.join(data_path, '16063x280x4_nmf_comparison.eps'), df=df2)
-    #plot(out_path=os.path.join(data_path, '54675x1973x4_nmf_comparison.eps'), df=df3)
+    plot(out_path=os.path.join(data_path, '5000x38x4_nmf_comparison.eps'), df=df1)
+    plot(out_path=os.path.join(data_path, '16063x280x4_nmf_comparison.png'), df=df2)
+    plot(out_path=os.path.join(data_path, '54675x1973x4_nmf_comparison.eps'), df=df3)
